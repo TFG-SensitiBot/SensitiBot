ad5e9977b808987aacff8147a1635fa190f50015
RingoML <josemanuelmartinluque@outlook.com>
2023-03-21 04:07:17 +0100

    Mejora de la interfaz usando opciones y argumentos

--- sensitibot/github.py
+++ sensitibot/github.py
@@ -17,20 +17,40 @@ def getRateLimit():
     return json_data
 
 def getFilesFromRepositories(owner):
+    TOKEN = os.getenv("GITHUB_TOKEN", default=None)
+
+    if TOKEN != None:
+        headers['Authorization'] = f'Bearer {TOKEN}'
+
     response = requests.get(f'{api_url}/users/{owner}/repos', headers=headers)
     json_repos = response.json()
 
     repositories = {}
 
     for repo in json_repos:
-        response = requests.get(f'{api_url}/repos/{owner}/{repo["name"]}/git/trees/{repo["default_branch"]}?recursive=1',headers=headers)
-        json_files = response.json()
-        
-        csv_files = []
-        for file in json_files["tree"]:
-            if(file["type"] == "blob" and file["path"].endswith(".csv")):
-                csv_files.append(f'{raw_url}/{owner}/{repo["name"]}/{repo["default_branch"]}/{file["path"]}')
-
+        csv_files = json.loads(getFilesFromRepository(owner, repo["name"], repo["default_branch"]))
         repositories[repo["name"]] = csv_files
 
-    return json.dumps(repositories)
+    return json.dumps(repositories, indent=4)
+
+def getFilesFromRepository(owner, repository, branch="null"):
+    TOKEN = os.getenv("GITHUB_TOKEN", default=None)
+
+    if TOKEN != None:
+        headers['Authorization'] = f'Bearer {TOKEN}'
+
+    # In case the branch is not specified, we need to get the default branch
+    if branch == "null":
+        response = requests.get(f'{api_url}/repos/{owner}/{repository}', headers=headers)
+        json_repo = response.json()
+        branch = json_repo["default_branch"]
+
+    response = requests.get(f'{api_url}/repos/{owner}/{repository}/git/trees/{branch}?recursive=1', headers=headers)
+    json_files = response.json()
+
+    csv_files = []
+    for file in json_files["tree"]:
+        if(file["type"] == "blob" and file["path"].endswith(".csv")):
+            csv_files.append(f'{raw_url}/{owner}/{repository}/master/{file["path"]}')
+
+    return json.dumps(csv_files, indent=4)
--- sensitibot/sensitibot.py
+++ sensitibot/sensitibot.py
@@ -8,13 +8,32 @@ load_dotenv()
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("owner")
+    #parser.add_argument("owner")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-l', '--local', metavar="PATH", const='./', nargs='?', help='use a local repository (default: current directory)')
+    group.add_argument('-g', '--github', metavar='USER', help='use a GitHub repository')
+    parser.add_argument('-r', '--repository', metavar='REPO',  help='the repository to use (only if --github is used)')
 
     args = parser.parse_args()
 
-    data = getFilesFromRepositories(args.owner)
+    if args.github:
+        if args.repository:
+            data = getFilesFromRepository(args.github, args.repository)
 
-    # Write data to file
-    os.makedirs("outputs", exist_ok=True)
-    with open('outputs/data.json', 'w') as f:
-        f.write(data)
+            # Write data to file
+            os.makedirs("outputs", exist_ok=True)
+            with open('outputs/data.json', 'w') as f:
+                f.write(data)
+        else:
+            data = getFilesFromRepositories(args.github)
+
+            # Write data to file
+            os.makedirs("outputs", exist_ok=True)
+            with open('outputs/data.json', 'w') as f:
+                f.write(data)
+
+    elif args.local:
+        if args.local == './' or args.local == None:
+            print("Using local repository: current directory")
+        else:
+            print(f"Using local repository: {args.local}")
