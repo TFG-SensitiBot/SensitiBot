b043fb31c16583678c9cc786f722606f21aa83c2
José Manuel Martín Luque <josemanuelmartinluque@outlook.com>
2023-06-01 01:38:36 +0200

    Mejora del diálogo de ayuda de la herramienta
    Mejora el custom formatter y añade un custom parser para los argumentos.

--- sensitibot/custom_formatters.py
+++ sensitibot/custom_formatters.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 
 
 class CustomHelpFormatter(argparse.HelpFormatter):
@@ -7,7 +8,11 @@ class CustomHelpFormatter(argparse.HelpFormatter):
             return super()._format_action_invocation(action)
         default = self._get_default_metavar_for_optional(action)
         args_string = self._format_args(action, default)
-        return ', '.join(action.option_strings) + ' ' + args_string
+
+        # Align the args_string with the maximum width
+        option_strings = (', '.join(action.option_strings)).ljust(17)
+
+        return option_strings + ' ' + args_string
 
     def format_help(self):
         help_text = super().format_help()
@@ -15,3 +20,10 @@ class CustomHelpFormatter(argparse.HelpFormatter):
 
     def __init__(self, prog):
         super().__init__(prog, max_help_position=40, width=100)
+
+
+class CustomArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        self.print_usage(sys.stderr)
+        error_message = message + '\n'  # Add an empty line after the error message
+        self.exit(2, '%s: error: %s\n' % (self.prog, error_message))
--- sensitibot/sensitibot.py
+++ sensitibot/sensitibot.py
@@ -1,10 +1,7 @@
 import argparse
 import sys
 from importlib.metadata import version
-
-import pkg_resources
-
-from sensitibot import help_formatter
+from sensitibot import custom_formatters
 from cleaner import cleaner
 from github import github
 from local import local
@@ -12,41 +9,45 @@ from renderer import renderer
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        prog='SensitiBot',
+    parser = custom_formatters.CustomArgumentParser(
+        prog='sensitibot',
         description='SensitiBot is a tool to analyze datasets for sensitive information.',
-        formatter_class=help_formatter.CustomHelpFormatter
+        formatter_class=custom_formatters.CustomHelpFormatter
     )
 
-    subparsers = parser.add_subparsers(dest='command')
+    subparsers = parser.add_subparsers(dest='command', metavar='subcommands:')
     parser.add_argument('-v', '--version', action='version',
                         version='%(prog)s ' + version('SensitiBot'))
 
     # sensitibot github
     github_parser = subparsers.add_parser(
-        'github', formatter_class=help_formatter.CustomHelpFormatter, help='Analyze GitHub repositories')
+        'github', formatter_class=custom_formatters.CustomHelpFormatter, help='analyze GitHub repositories')
     github_parser.add_argument('user', type=str, metavar='USER',
-                               help='The GitHub user or organization to analyze')
+                               help='the GitHub user or organization to analyze')
     github_parser.add_argument('-r', '--repository', metavar='REPO',
-                               help='Analyze a specific repository')
+                               help='analyze a specific repository')
     github_parser.add_argument('-b', '--branch', metavar='BRANCH',
-                               help='Analyze a specific branch (only if repository is specified)')
+                               help='analyze a specific branch (only if repository is specified)')
     github_parser.add_argument('-t', '--token', metavar='TOKEN',
-                               help='The GitHub token to use for authentication')
+                               help='the GitHub token to use for authentication')
     github_parser.add_argument('--deep-search', action='store_true',
-                               help='Analyze content of files')
+                               help='analyze content of files')
     github_parser.add_argument('--wide-search', action='store_true',
-                               help='Analyze all tables or sheets in Office files')
+                               help='analyze all tables or sheets in Office files')
 
     # sensitibot local
     local_parser = subparsers.add_parser(
-        'local', formatter_class=help_formatter.CustomHelpFormatter, help='Analyze local repository')
+        'local', formatter_class=custom_formatters.CustomHelpFormatter, help='analyze local repository')
     local_parser.add_argument('path', type=str, metavar='PATH',
                               const='./', nargs='?', help='The path to analyze')
     local_parser.add_argument('--deep-search', action='store_true',
-                              help='Analyze content of files')
+                              help='analyze content of files')
     local_parser.add_argument('--wide-search', action='store_true',
-                              help='Analyze all tables or sheets in Office files')
+                              help='analyze all tables or sheets in Office files')
+
+    parser._optionals.title = "optional arguments"
+    github_parser._optionals.title = "optional arguments"
+    local_parser._optionals.title = "optional arguments"
 
     args = parser.parse_args()
 
